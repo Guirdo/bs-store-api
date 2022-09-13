80611b158066e6c483e02631cb654c4d54e88fa1
Guirdo <produccionesaldo@gmail.com>
2022-09-13 13:56:31 -0500

    Pagination improved

--- services/product.js
+++ services/product.js
@@ -4,42 +4,85 @@ const db = require('./db')
 
 const getAllProduct = async (page = 1) => {
     const offset = getOffset(page, config.limitPerPage)
-    const rows = await db.query(`
-        SELECT * FROM product
+    const products = await db.query(`
+        SELECT *
+        FROM product
         LIMIT ${config.limitPerPage} OFFSET ${offset}
     `)
 
-    return rows
+    const pagination = await getPagination(`
+        SELECT count(*) as totalOfItems
+        FROM product
+    `, page)
+
+    return { pagination, products }
 }
 
 const getProductByCategory = async (id, page = 1) => {
     const offset = getOffset(page, config.limitPerPage)
-    const rows = await db.query(`
-        SELECT * FROM product WHERE category = ${id}
+    const products = await db.query(`
+        SELECT *
+        FROM product 
+        WHERE category = ${id}
         LIMIT ${config.limitPerPage} OFFSET ${offset}
     `)
 
-    return rows
+    const pagination = await getPagination(`
+        SELECT count(*) as totalOfItems
+        FROM product 
+        WHERE category = ${id}
+    `, page)
+
+    return { pagination, products }
 }
 
-const searchProduct = async (input,page = 1) => {
+const searchProduct = async (input, page = 1) => {
     const offset = getOffset(page, config.limitPerPage)
     let words = input.split(' ')
-    let rows
-    
+    let products
+    let pagination
+
     if (words.length > 1) {
-        rows = await db.query(`
-            SELECT * FROM product where name like '%${input}%'
+        products = await db.query(`
+            SELECT *
+            FROM product 
+            WHERE name like '%${input}%'
             LIMIT ${config.limitPerPage} OFFSET ${offset}
         `)
-    } else{
-        rows = await db.query(`
-            SELECT * FROM product where name like '${input}%'
+
+        pagination = await getPagination(`
+            SELECT count(*) as totalOfItems
+            FROM product 
+            WHERE name like '%${input}%'
+        `, page)
+    } else {
+        products = await db.query(`
+            SELECT *
+            FROM product 
+            WHERE name like '${input}%'
             LIMIT ${config.limitPerPage} OFFSET ${offset}
         `)
+
+        pagination = await getPagination(`
+            SELECT count(*) as totalOfItems
+            FROM product 
+            WHERE name like '${input}%'
+        `, page)
     }
 
-    return rows
+    return { pagination, products }
+}
+
+const getPagination = async (query, currentPage) => {
+    const [{ totalOfItems }] = await db.query(query)
+    const numberOfPages = Math.ceil(totalOfItems / config.limitPerPage)
+
+    return {
+        totalOfItems,
+        limitPerPage: config.limitPerPage,
+        currentPage: parseInt(currentPage),
+        numberOfPages
+    }
 }
 
 module.exports = {
